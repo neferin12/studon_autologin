f50526b3563810d8852c7d6697b3d5df39579471
Julian Pollinger <julian@pollinger.dev>
2023-09-17 18:37:27 +0200

    v1.1

--- login.js
+++ login.js
@@ -3,8 +3,10 @@ if(login_button){
     console.debug("Login button found, clicking it...")
     login_button.click()
 }
-login_button = document.querySelector("a[href='saml.php']")
+login_button = document.querySelector("a[href='./saml.php']") || document.evaluate("//a[contains(., 'Anmelden')]", document, null, XPathResult.ANY_TYPE, null ).iterateNext()
 if(login_button){
     console.debug("Login button found, clicking it...")
     login_button.click()
+}else{
+    console.warn("Login button not found!")
 }
