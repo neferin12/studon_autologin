5b715e61b626834fe92e30b1bda63d46f0ca852e
Julian Pollinger <julian@pollinger.dev>
2024-02-17 15:13:47 +0100

    login directly

--- login.js
+++ login.js
@@ -1,12 +1,24 @@
-let login_button = document.getElementsByClassName("glyphicon-login")[0]
-if(login_button){
-    console.debug("Login button found, clicking it...")
-    login_button.click()
+function login() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const target = urlParams.get('target');
+
+    let samlUrl = "./saml.php";
+    if(target) {
+        samlUrl += "?returnTo=" + target;
+    }
+    window.open(samlUrl,"_self")
 }
-login_button = document.querySelector("a[href='./saml.php']") || document.evaluate("//a[contains(., 'Anmelden')]", document, null, XPathResult.ANY_TYPE, null ).iterateNext()
-if(login_button){
-    console.debug("Login button found, clicking it...")
-    login_button.click()
-}else{
-    console.warn("Login button not found!")
+
+function checkForLoginButton() {
+    return (
+        !!(
+            document.getElementsByClassName("glyphicon-login")[0] ||
+            document.querySelector("a[href='./saml.php']") ||
+            document.evaluate("//a[contains(., 'Anmelden')]", document, null, XPathResult.ANY_TYPE, null).iterateNext()
+        )
+    )
+}
+
+if(checkForLoginButton()) {
+    login()
 }
